8bf58ceec8fd1ed76cf8718355e8238bbd14c2ff
nkumar2 <nkumar2@ebi.ac.uk>
2024-05-20 12:18:50 +0100

    remove the option of providing csv mapping through command line and instead use metadata files

--- bin/eva-sub-cli.py
+++ bin/eva-sub-cli.py
@@ -16,11 +16,6 @@ from eva_sub_cli.file_utils import is_submission_dir_writable
 
 
 def validate_command_line_arguments(args, argparser):
-    if args.vcf_files_mapping and (args.vcf_files or args.assembly_fasta):
-        print("Specify vcf_files and assembly_fasta OR a vcf_files_mapping in CSV. Not both")
-        argparser.print_usage()
-        sys.exit(1)
-
     if (args.vcf_files and not args.assembly_fasta) or (not args.vcf_files and args.assembly_fasta):
         print("When using --vcf_files and --assembly_fasta, both need to be specified")
         argparser.print_usage()
@@ -58,8 +53,6 @@ if __name__ == "__main__":
     vcf_group.add_argument('--vcf_files', nargs='+', help="One or several vcf files to validate")
     vcf_group.add_argument('--assembly_fasta',
                            help="The fasta file containing the reference genome from which the variants were derived")
-    vcf_group.add_argument("--vcf_files_mapping",
-                           help="csv file with the mappings for vcf files, fasta and assembly report")
 
     metadata_group = argparser.add_argument_group('Metadata', 'Specify the metadata in a spreadsheet or in a JSON file')
     metadata_group = metadata_group.add_mutually_exclusive_group(required=True)
