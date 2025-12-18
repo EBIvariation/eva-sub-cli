38692bbff8ba4180f9cfe89e23edde5749fb2b2c
April Shen <ashen@ebi.ac.uk>
2025-12-18 14:10:19 +0000

    remove unused import and update documentation

--- tests/test_orchestrator.py
+++ tests/test_orchestrator.py
@@ -3,7 +3,6 @@ import json
 import os
 import shutil
 import unittest
-from copy import deepcopy
 from unittest.mock import patch, Mock, MagicMock
 
 from ebi_eva_common_pyutils.config import WritableConfig
