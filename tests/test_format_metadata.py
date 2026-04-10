465537d901d9fc3db9538c814b35cb3ca2e7c110
khetherin <hetherin@ebi.ac.uk>
2026-04-10 16:32:10 +0100

    fix(test_format_metadata.py): edit tests

--- tests/test_format_metadata.py
+++ tests/test_format_metadata.py
@@ -17,11 +17,14 @@ class TestValidateMetadata(TestCase):
             "sample": [{"analysisAlias": [""], "sampleInVCF": "", "bioSampleObject": {"sample_title": "", "scientific_name": "", "tax_id": 9606, "collection date": "not provided", "geographic location (country and/or sea)": "not provided"}}],
             "files": [{"analysisAlias": "", "fileName": "", "fileSize": 1000, "md5": ""}]
         }
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_out_clean:
+            temp_path_clean_output = tmp_out_clean.name
+            tmp_out_clean.write('')
         with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp:
             json.dump(clean_json, tmp)
             temp_path_clean = tmp.name
-        validated_result = format_metadata.format_experiment_type_lowercase(temp_path_clean)
-        with open(validated_result, 'r') as f:
+        format_metadata.format_experiment_type_lowercase(temp_path_clean, temp_path_clean_output)
+        with open(temp_path_clean_output, 'r') as f:
             result_data = json.load(f)
         expected = clean_json
         self.assertEqual(result_data, expected)
@@ -36,11 +39,13 @@ class TestValidateMetadata(TestCase):
             "sample": [{"analysisAlias": [""], "sampleInVCF": "", "bioSampleObject": {"sample_title": "", "scientific_name": "", "tax_id": 9606, "collection date": "not provided", "geographic location (country and/or sea)": "not provided"}}],
             "files": [{"analysisAlias": "", "fileName": "", "fileSize": 1000, "md5": ""}]
         }
+        with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_out_unclean:
+            temp_path_unclean_output = tmp_out_unclean.name
         with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp:
             json.dump(unclean_json, tmp)
             temp_path_unclean = tmp.name
-        validated_result = format_metadata.format_experiment_type_lowercase(temp_path_unclean)
-        with open(validated_result, 'r') as f:
+        format_metadata.format_experiment_type_lowercase(temp_path_unclean, temp_path_unclean_output)
+        with open(temp_path_unclean_output, 'r') as f:
             result_data = json.load(f)
         expected = unclean_json
         self.assertNotEqual(result_data, expected)
