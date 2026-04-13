83c3fdd4006439f7bd880fd9dfefb3506ede212a
khetherin <hetherin@ebi.ac.uk>
2026-04-13 12:41:01 +0100

    refactor(format_metadata.py, test_format_metadata.py): add experiment type mapping and edit tests

--- eva_sub_cli/executables/format_metadata.py
+++ eva_sub_cli/executables/format_metadata.py
@@ -4,13 +4,25 @@ import json
 from ebi_eva_common_pyutils.logger import logging_config
 logger = logging_config.get_logger(__name__)
 
-def format_experiment_type_lowercase(json_file_input, json_file_output):
+def format_experiment_type(json_file_input, json_file_output):
     with open(json_file_input, 'r') as input:
         json_data = json.load(input)
 
+    experiment_type_mapping = {
+        "whole genome sequencing": "Whole genome sequencing",
+        "whole transcriptome sequencing": "Whole transcriptome sequencing",
+        "exome sequencing": "Exome sequencing",
+        "genotyping by array": "Genotyping by array",
+        "curation": "Curation",
+        "genotyping by sequencing": "Genotyping by sequencing",
+        "target sequencing": "Target sequencing",
+        "transcriptomics": "Transcriptomics"
+    }
     for index in range(len(json_data["analysis"])):
         logger.info(f"Formatting metadata - experiment type to lower case - {json_data['analysis'][index]['experimentType']}")
-        json_data["analysis"][index]["experimentType"] = json_data["analysis"][index]["experimentType"].lower()
+        experiment_type_lower_case = json_data["analysis"][index]["experimentType"].lower()
+        experiment_type = experiment_type_mapping[experiment_type_lower_case]
+        json_data["analysis"][index]["experimentType"] = experiment_type
 
     with open(json_file_output, 'w') as output:
         json.dump(json_data, output, indent=4)
@@ -24,7 +36,7 @@ def main():
     arg_parser.add_argument('--metadata_json_output', required=True, dest='metadata_json_output',
                             help='EVA metadata json file OUTPUT')
     args = arg_parser.parse_args()
-    format_experiment_type_lowercase(args.metadata_json_input, args.metadata_json_output)
+    format_experiment_type(args.metadata_json_input, args.metadata_json_output)
 
 if __name__ == "__main__":
     main()
--- tests/test_format_metadata.py
+++ tests/test_format_metadata.py
@@ -6,13 +6,13 @@ import json
 
 class TestValidateMetadata(TestCase):
 
-    def test_format_experiment_type_lowercase(self):
+    def test_format_experiment_type(self):
         clean_json =  {
             "submitterDetails": [{"lastName": "", "firstName": "", "email": "", "laboratory": "", "centre": ""}],
             "project": {"title": "", "description": "", "taxId": 9606, "centre": ""},
             "analysis": [{"analysisTitle": "", "analysisAlias": "", "description": "",
                     # CHANGING THE LINE BELOW
-                    "experimentType": "genotyping_by_array",
+                    "experimentType": "Genotyping by array",
                     "referenceGenome": ""}],
             "sample": [{"analysisAlias": [""], "sampleInVCF": "", "bioSampleObject": {"sample_title": "", "scientific_name": "", "tax_id": 9606, "collection date": "not provided", "geographic location (country and/or sea)": "not provided"}}],
             "files": [{"analysisAlias": "", "fileName": "", "fileSize": 1000, "md5": ""}]
@@ -23,7 +23,7 @@ class TestValidateMetadata(TestCase):
         with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp:
             json.dump(clean_json, tmp)
             temp_path_clean = tmp.name
-        format_metadata.format_experiment_type_lowercase(temp_path_clean, temp_path_clean_output)
+        format_metadata.format_experiment_type(temp_path_clean, temp_path_clean_output)
         with open(temp_path_clean_output, 'r') as f:
             result_data = json.load(f)
         expected = clean_json
@@ -34,7 +34,7 @@ class TestValidateMetadata(TestCase):
             "project": {"title": "", "description": "", "taxId": 9606, "centre": "",},
             "analysis": [{"analysisTitle": "", "analysisAlias": "", "description": "",
                     # CHANGING THE LINE BELOW
-                    "experimentType": "Genotyping_by_array",
+                    "experimentType": "GeNoTyPiNg By ArRaY",
                     "referenceGenome": ""}],
             "sample": [{"analysisAlias": [""], "sampleInVCF": "", "bioSampleObject": {"sample_title": "", "scientific_name": "", "tax_id": 9606, "collection date": "not provided", "geographic location (country and/or sea)": "not provided"}}],
             "files": [{"analysisAlias": "", "fileName": "", "fileSize": 1000, "md5": ""}]
@@ -44,7 +44,7 @@ class TestValidateMetadata(TestCase):
         with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp:
             json.dump(unclean_json, tmp)
             temp_path_unclean = tmp.name
-        format_metadata.format_experiment_type_lowercase(temp_path_unclean, temp_path_unclean_output)
+        format_metadata.format_experiment_type(temp_path_unclean, temp_path_unclean_output)
         with open(temp_path_unclean_output, 'r') as f:
             result_data = json.load(f)
         expected = unclean_json
