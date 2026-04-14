01dc19518c566bc62b8a0b9e3b9f1154efb5dbe0
khetherin <hetherin@ebi.ac.uk>
2026-04-14 14:35:58 +0100

    refactor(test_format_metadata.py): remove weak tests and clean up files

--- tests/test_format_metadata.py
+++ tests/test_format_metadata.py
@@ -1,4 +1,4 @@
-
+import os.path
 from unittest import TestCase
 from eva_sub_cli.executables import format_metadata
 import tempfile
@@ -7,12 +7,15 @@ import json
 class TestValidateMetadata(TestCase):
 
     def test_format_experiment_type(self):
+        expected_experiment_type = "Genotyping by array"
+        unexpected_experiment_type = "GeNoTyPiNg By ArRaY"
+        # testing a correctly formatted JSON file
         clean_json =  {
             "submitterDetails": [{"lastName": "", "firstName": "", "email": "", "laboratory": "", "centre": ""}],
             "project": {"title": "", "description": "", "taxId": 9606, "centre": ""},
             "analysis": [{"analysisTitle": "", "analysisAlias": "", "description": "",
                     # CHANGING THE LINE BELOW
-                    "experimentType": "Genotyping by array",
+                    "experimentType": expected_experiment_type,
                     "referenceGenome": ""}],
             "sample": [{"analysisAlias": [""], "sampleInVCF": "", "bioSampleObject": {"sample_title": "", "scientific_name": "", "tax_id": 9606, "collection date": "not provided", "geographic location (country and/or sea)": "not provided"}}],
             "files": [{"analysisAlias": "", "fileName": "", "fileSize": 1000, "md5": ""}]
@@ -28,13 +31,18 @@ class TestValidateMetadata(TestCase):
             result_data = json.load(f)
         expected = clean_json
         self.assertEqual(result_data, expected)
-
+        self.assertEqual(result_data["analysis"][0]["experimentType"], expected_experiment_type)
+        if os.path.exists(temp_path_clean_output):
+            os.remove(temp_path_clean_output)
+        if os.path.exists(temp_path_clean):
+            os.remove(temp_path_clean)
+        # testing an incorrectly formatted JSON file
         unclean_json = {
             "submitterDetails": [{"lastName": "", "firstName": "", "email": "", "laboratory": "", "centre": ""}],
             "project": {"title": "", "description": "", "taxId": 9606, "centre": "",},
             "analysis": [{"analysisTitle": "", "analysisAlias": "", "description": "",
                     # CHANGING THE LINE BELOW
-                    "experimentType": "GeNoTyPiNg By ArRaY",
+                    "experimentType": unexpected_experiment_type,
                     "referenceGenome": ""}],
             "sample": [{"analysisAlias": [""], "sampleInVCF": "", "bioSampleObject": {"sample_title": "", "scientific_name": "", "tax_id": 9606, "collection date": "not provided", "geographic location (country and/or sea)": "not provided"}}],
             "files": [{"analysisAlias": "", "fileName": "", "fileSize": 1000, "md5": ""}]
@@ -47,5 +55,10 @@ class TestValidateMetadata(TestCase):
         format_metadata.format_experiment_type(temp_path_unclean, temp_path_unclean_output)
         with open(temp_path_unclean_output, 'r') as f:
             result_data = json.load(f)
-        expected = unclean_json
-        self.assertNotEqual(result_data, expected)
+        expected = clean_json
+        self.assertEqual(result_data, expected)
+        self.assertEqual(result_data["analysis"][0]["experimentType"], expected_experiment_type)
+        if os.path.exists(temp_path_unclean_output):
+            os.remove(temp_path_unclean_output)
+        if os.path.exists(temp_path_unclean):
+            os.remove(temp_path_unclean)
